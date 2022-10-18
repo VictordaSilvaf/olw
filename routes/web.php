ac8fc24ccbe4316402f64ac74927742fe9735749
VictordaSilvaf <victordasilvafernandes@gmail.com>
2022-10-18 15:44:33 -0300

    Final aula 1

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,9 @@
 <?php
 
+use App\Http\Controllers\BeerController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -28,4 +18,14 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/profile', function () {
+    return Inertia::render('Profile');
+})->middleware(['auth', 'verified'])->name('profile');
+
 require __DIR__.'/auth.php';
+
+Route::group(['prefix' => 'beers'], function () {
+    Route::get('/', [BeerController::class, 'index'])->name('beers.index')->middleware(['auth', 'verified']);
+    Route::get('/export', [BeerController::class, 'export'])->name('beers.export')->middleware(['auth', 'verified']);
+});
+
